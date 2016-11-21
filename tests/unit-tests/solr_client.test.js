d8dfeae4ded2c9d0aa5eb8242d3e9ddbfc44405f
michaelk <michaelk@iron-buzz.com>
2016-11-21 11:16:28 +0200

    update

--- tests/unit-tests/solr_client.test.js
+++ tests/unit-tests/solr_client.test.js
@@ -14,7 +14,7 @@ describe("Solr client test", function()
         return next();
     })
 
-    it("test1", function (next)
+    it("test12", function (next)
     {
         var solrUrl = "http://localhost:8983/solr/ML";
         var solrClient = new SolrClient(solrUrl);
